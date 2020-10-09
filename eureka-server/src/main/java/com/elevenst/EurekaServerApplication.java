c38b605090ebe6955e09893e1d93d30cf0a07845
nick <km.oh@dreamus.io>
2020-10-09 17:47:02 +0900

    Eureka 설정 및 테스트

---
+++ eureka-server/src/main/java/com/elevenst/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.elevenst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Nick (km.oh@dreamus.io)
+ * @created 2020-10-09
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
